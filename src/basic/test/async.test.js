0130dbd2412c54d17bd6496a8a2c7029969e6ed3
songseunghyun <songsh96927@gmail.com>
2022-07-04 23:32:15 +0900

    .

---
+++ src/basic/test/async.test.js
@@ -0,0 +1,29 @@
+const fetchProduct = require('../async.js');
+
+describe('Async' , () => {
+    it('async - done' , (done) => {
+        fetchProduct().then((item) => {
+            expect(item).toEqual({item: 'Milk', price: 2000});
+            done();
+        });
+    });
+
+    it('async - return' , () => {
+        return fetchProduct().then((item) => {
+            expect(item).toEqual({item: 'Milk', price: 2000});
+        });
+    });
+
+    it('async - await' , async () => {
+        const product = await fetchProduct();
+        expect(product).toEqual({item: 'Milk', price: 2000})
+    });
+
+    it('async - resolves', () => {
+        return expect(fetchProduct()).resolves.toEqual({item: 'Milk', price: 2000});
+    });
+    
+    it('async - resject', () => {
+        return expect(fetchProduct('error')).rejects.toBe('network error');
+    });
+});
